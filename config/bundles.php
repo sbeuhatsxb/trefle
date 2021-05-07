ba1a2e4e0085370d9b6b8218e3e602f8debc6879
Sebastien STELTZLEN <sebastien.steltzlen@proximis.com>
2021-05-07 17:23:55 +0200

    API Platform installed

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];

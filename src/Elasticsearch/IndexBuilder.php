21ab75f2a5cd65a4775af96c79507d659e6b4a1b
sbeuhatsxb <mail2sbeuh@gmail.com>
2021-05-15 10:12:02 +0200

    Scalingo config

--- src/Elasticsearch/IndexBuilder.php
+++ src/Elasticsearch/IndexBuilder.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Elasticsearch;
 
 use Elastica\Client;
@@ -18,14 +19,59 @@ class IndexBuilder
     {
         // We name our index "plant"
         $index = $this->client->getIndex('plantapi');
-        $settings = Yaml::parse(
-            file_get_contents(
-                __DIR__.'/../../config/elasticsearch/plant_mapping.yaml'
-            )
-        );
-        // We build our index settings and mapping
-        $index->create($settings, true);
+        //        $settings = Yaml::parse(
+        //            file_get_contents(
+        //                __DIR__.'/../../config/elasticsearch/plant_mapping.yaml'
+        //            )
+        //        );
 
+        // We build our index settings and mapping
+        //$index->create($settings, true);
+        $index->create(array(
+            'settings' =>
+                array(
+                    'index' =>
+                        array(
+                            'number_of_shards' => 1,
+                            'number_of_replicas' => 0,
+                            'refresh_interval' => '60s',
+                        ),
+                )
+            ),
+ true);
+
+        $mapping = new \Elastica\Mapping();
+        $mapping->setProperties(
+                array(
+                    'dynamic' => false,
+                    'properties' =>
+                        array(
+                            'scientific_name' =>
+                                array(
+                                    'type' => 'text',
+                                ),
+                            'common_name' =>
+                                array(
+                                    'type' => 'text',
+                                    'analyzer' => 'english',
+                                ),
+                            'family_common_name' =>
+                                array(
+                                    'type' => 'text',
+                                ),
+                            'synonyms' =>
+                                array(
+                                    'type' => 'text',
+                                ),
+                            'common_names' =>
+                                array(
+                                    'type' => 'text',
+                                ),
+                        ),
+                ),
+        );
+
+        $mapping->send();
         return $index;
     }
 }

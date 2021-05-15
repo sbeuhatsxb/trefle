c91dfa973f1fa97d41125d192e1bba4f20e89976
sbeuhatsxb <mail2sbeuh@gmail.com>
2021-05-15 08:56:37 +0200

    Scalingo config

--- src/Command/ElasticReindexCommand.php
+++ src/Command/ElasticReindexCommand.php
@@ -44,8 +44,7 @@ class ElasticReindexCommand extends Command
 
         $io->success('Index created!');
         $io->success($index->getName());
-        $io->success($this->plantIndexer->indexAllDocuments($index->getName()));
-
+        
         $this->plantIndexer->indexAllDocuments($index->getName());
 
         $io->success('Index populated and ready!');

27ff7d2ad1becc8d5208f5bce5ff19f257980e19
sbeuhatsxb <mail2sbeuh@gmail.com>
2021-05-16 09:35:06 +0200

    Scalingo config

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -27,10 +27,8 @@ class TestController extends AbstractController
     {
         $client = new Client();
 
-        $client->connect();
 
-
-        $response = new Response(json_encode($client->getStatus()->getIndexNames()), 200);
+        $response = new Response(json_encode($client->getConfig('host'), $client->getConfig('port'), $client->getConfig('username')), 200);
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;

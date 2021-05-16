dcb6a122448233e42e96696d18c6d3eab54e39f7
sbeuhatsxb <mail2sbeuh@gmail.com>
2021-05-16 08:02:38 +0200

    Scalingo config

---
+++ src/Controller/TestController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Elasticsearch\IndexBuilder;
+use App\Elasticsearch\PlantIndexer;
+use App\Entity\Plant;
+use App\Entity\Token;
+use App\Repository\TokenRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Elastica\Client;
+use Elastica\Document;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/test", methods={"GET"})
+     * @return Response
+     */
+    public function test(): Response
+    {
+        $client = new Client();
+
+        $index = $client->getIndex('test');
+        $index->create(array(), true);
+        $type->addDocument(new Document(1, array('username' => 'ruflin'), "_doc"));
+        $index->refresh();
+
+        $query = '{"query":{"query_string":{"query":"ruflin"}}}';
+
+        $path = $index->getName() . '/' . $type->getName() . '/_search';
+
+        $response = $client->request($path, Request::METHOD_GET, $query);
+        $responseArray = $response->getData();
+
+
+        $response = new Response(json_encode($responseArray), 200);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+
+    }
+
+}
